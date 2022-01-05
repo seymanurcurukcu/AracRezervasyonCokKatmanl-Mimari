77dc7d72b07e248294856ea562217a69a4737dc9
Şeymanur Çürükcü <55655002+seymanurcurukcu@users.noreply.github.com>
2022-01-05 17:09:15 +0300

    businessVeEntitykatmanlari

---
+++ EntityLayer/Entities/MusteriAracKiralama.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    class MusteriAracKiralama
+    {
+        public Arac AracMak { get; set; }
+        public Musteri MusteriMak { get; set; }
+        public Kiralik KiralikMak { get; set; }
+    }
+}
